9cfd371c9c5d5de9dc21f8329cdadb4a3e1b3aff
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 18:22:39 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,7 +82,8 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -102,7 +105,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+programmer_hash[:alan_kay][:known_for] = "GUI" 
+programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 end
